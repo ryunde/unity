8e2bc88d2995c1b5ceb668eaf7ad023de30226aa
ryunde <ryunde.taba@gmail.com>
2016-03-03 23:31:15 +0200

    camera follows player

--- CameraFollow.cs
+++ CameraFollow.cs
@@ -7,14 +7,12 @@ public class CameraFollow : MonoBehaviour {
 	public float m_speed = 0.1f;
 	Camera mycam;
 
-	// Use this for initialization
 	void Start () {
 	
 		mycam = GetComponent<Camera> ();
 
 	}
 	
-	// Update is called once per frame
 	void Update () {
 	
 		mycam.orthographicSize = (Screen.height / 100f) / 4f;
